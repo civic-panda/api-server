cbf4430ab6ad2e45c96f7151ed8750c80f9785f0
Matt Casey <matt@mattcasey.nyc>
2016-12-02 16:26:38 -0500

    cors package

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var request = require('request');
+var cors = require('cors');
 
 var apiServerHost = 'https://congress.api.sunlightfoundation.com';
 var app = express();
+
+app.use(cors());
+
+app.use('/test', function(req, res) {
+  res.send('hello world');
+});
 
 app.use('/', function(req, res) {
   var url = apiServerHost + req.url;
