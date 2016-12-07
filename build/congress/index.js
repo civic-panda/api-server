1142a08f13434fdfe7865307f5ad57981aed95c4
Matt Casey <matt@mattcasey.nyc>
2016-12-06 21:54:46 -0500

    fixed build process

--- build/congress/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const congress = require('../../data/legislators-current.json');
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = congress;
-//# sourceMappingURL=index.js.map
