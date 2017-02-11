1d714b01af1e09edf128427e3fb4b091b3841127
Matt Casey <matt@mattcasey.nyc>
2017-02-10 23:12:04 -0500

    cargo-culting the shit out of this

--- src/index.ts
+++ src/index.ts
@@ -131,6 +131,6 @@ app.use((err: any, _req: any, res: any, next:any) => {
   return next();
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8081;
 app.listen(port);
 console.log(`listening on port ${port}`);
